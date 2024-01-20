a21bfb94c393491b6aced75a613fa131853a63b7
CamposCaio <camposcaioh@gmail.com>
2024-01-20 10:59:06 -0300

    feat: :construction: WIP add docker compose to project

--- database-writer/src/kafka/kafka.config.ts
+++ database-writer/src/kafka/kafka.config.ts
@@ -3,5 +3,5 @@ import { KafkaConfig } from 'kafkajs';
 export const KAFKA_CONFIG = Symbol('KAFKA_CONFIG');
 
 export const kafkaConfig: KafkaConfig = {
-  brokers: ['localhost:9092'],
+  brokers: ['kafka1:29092'],
 };
--- iot-gateway/src/kafka/kafka.config.ts
+++ iot-gateway/src/kafka/kafka.config.ts
@@ -3,5 +3,11 @@ import { KafkaConfig } from 'kafkajs';
 export const KAFKA_CONFIG = Symbol('KAFKA_CONFIG');
 
 export const kafkaConfig: KafkaConfig = {
-  brokers: ['localhost:9092'],
+  clientId: 'iot-gateway',
+  brokers: ['kafka1:29092'],
+  ssl: false,
+  connectionTimeout: 20000,
+  retry: {
+    retries: 8,
+  },
 };
